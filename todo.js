f022e9841e045d97be5090fb0752e4616eb02888
MatthewClarkson <mattclarkson@email.com>
2024-01-02 09:08:04 -0500

    Added comments

--- todo.js
+++ todo.js
@@ -1,20 +1,20 @@
 let id = 0;
-
+                                    //Add button add click event
 document.getElementById('add').addEventListener('click', () => {
-  let createdDate = new Date();
+  let createdDate = new Date();     //To make new date and insert on list
   let table = document.getElementById('list');
-  let row = table.insertRow(1);
+  let row = table.insertRow(1);     
   row.setAttribute('id', `item-${id}`);
   row.insertCell(0).innerHTML = document.getElementById('new-task').value;
   row.insertCell(1).innerHTML = `${createdDate.getFullYear()}-${createdDate.getMonth() + 1}-${createdDate.getDate()}`;
   row.insertCell(2).innerHTML = document.getElementById('new-start-date').value;
   row.insertCell(3).innerHTML = document.getElementById('new-end-date').value;
-  let actions = row.insertCell(4);
-  actions.appendChild(createDeleteButton(id++));
-  document.getElementById('new-task').value = '';
+  let actions = row.insertCell(4);    //Insert new row cells for th values
+  actions.appendChild(createDeleteButton(id++));  //Create delete button
+  document.getElementById('new-task').value = ''; //New task
 });
 
-function createDeleteButton(id) {
+function createDeleteButton(id) {   //Primary delete button
     let btn = document.createElement('button');
     btn.className = 'btn btn-primary';
     btn.id = id;
